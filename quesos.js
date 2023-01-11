593932eaa0a4c8db7c68fa43552625a5044f2782
Aurino <a_llera_rodriguez@hotmail.com>
2023-01-11 11:55:55 +0100

    change1

--- quesos.js
+++ quesos.js
@@ -1,25 +1,81 @@
-const shopcontent=document.getElementById("prueba"); //aqui muestra el div prueba del html , coge el elemento por id//
+//const shopcontent=document.getElementById("prueba"); //aqui muestra el div prueba del html , coge el elemento por id//
 const queso=[  //1ero creamos el array un json que dentro va el id y el nombre
     {
         id:1,
-        nombre:"hola",
-        images : "img/usuario.png",
-
-        
+        nombre:"Queso rojo de cabra",
+        descripcion:"Se hace con leche entera, sin pasteurizar, de gran riqueza en mantequilla y, si es posible procedente de la raza bovina auctóctona de la montaña",
+        foto :"img/quesorojocabra.jpg",
+        precio:9.10,
+        categoria:"A",
+   
     },
     {
         id:2,
-        nombre:"adios",
-        images : "img/francia.png",
+        nombre:"Queso oveja bredo",
+        descripcion:"Se hace con leche entera, sin pasteurizar, de gran riqueza en mantequilla y, si es posible procedente de la raza bovina auctóctona de la montaña",
+        foto :"img/quesoovejaabredo.jpg",
+        precio:9.10,
+        categoria:"A"
+     
+    },
+    {
+        id:3,
+        nombre:"Queso maduro tres leches",
+        descripcion:"Se hace con leche entera, sin pasteurizar, de gran riqueza en mantequilla y, si es posible procedente de la raza bovina auctóctona de la montaña",
+        foto :"img/quesomadurotresleches.jpg",
+        precio:9.10,
+        categoria:"B"
+     
+    },
+    {
+        id:4,
+        nombre:"Queso oveja la collada",
+        descripcion:"Se hace con leche entera, sin pasteurizar, de gran riqueza en mantequilla y, si es posible procedente de la raza bovina auctóctona de la montaña",
+        foto :"img/quesoovejalacollada.jpg",
+        precio:9.10,
+     
+    },
+    {
+        id:5,
+        nombre:"Queso de coan",
+        descripcion:"Se hace con leche entera, sin pasteurizar, de gran riqueza en mantequilla y, si es posible procedente de la raza bovina auctóctona de la montaña",
+        foto :"img/quesodecoan.jpg",
+        precio:9.10,
+     
+    },
+    {
+        id:6,
+        nombre:"Queso Cabrales",
+        descripcion:"Se hace con leche entera, sin pasteurizar, de gran riqueza en mantequilla y, si es posible procedente de la raza bovina auctóctona de la montaña",
+        foto :"img/quesocabrales.jpg",
+        precio:9.10,
+     
+    },
+    {
+        id:7,
+        nombre:"Queso Azul de vaca",
+        descripcion:"Se hace con leche entera, sin pasteurizar, de gran riqueza en mantequilla y, si es posible procedente de la raza bovina auctóctona de la montaña",
+        foto :"img/quesoazulvaca.jpg",
+        precio:9.10,
+     
+    },
+    {
+        id:8,
+        nombre:"Afuegalpitu",
+        descripcion:"Se hace con leche entera, sin pasteurizar, de gran riqueza en mantequilla y, si es posible procedente de la raza bovina auctóctona de la montaña",
+        foto :"img/quesofuegalpitu.jpg",
+        precio:9.10,
+        categoria:"H",
+     
     },
 ];
-queso.forEach((product)=>{ //recorremos todo el array por el foreach y cada vez que recorremos el array nos crean en htm un h3 y le metemos el nombre del producto
-    let content=document.createElement("div");
+//queso.forEach((product)=>{ //recorremos todo el array por el foreach y cada vez que recorremos el array nos crean en htm un h3 y le metemos el nombre del producto
+  //  let content=document.createElement("div");
     //content.innerHTML=`<h3>${product.id}</h3>
-    content.innerHTML=`<img src ="${product.images}"> 
-    `; //creando etiqueta imagen . 
-shopcontent.append(content); //el resultado que te da es para meterlo en un div padre que se crea en html primero, se ve en inspeccionar elemento 
-});
+  //  content.innerHTML=`<img src ="${product.images}"> 
+  //  `; //creando etiqueta imagen . 
+//shopcontent.append(content); //el resultado que te da es para meterlo en un div padre que se crea en html primero, se ve en inspeccionar elemento 
+//});
 
 
 //ESTO ES UNA PRUEBA //
