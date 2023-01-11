f78f2d4f411a1324ffb1ef310efdf241f4c32389
Kanzaby <jenniferperezoblanca@hotmail.com>
2023-01-11 09:49:43 +0100

    createproducts

--- quesos.js
+++ quesos.js
@@ -1,25 +1,71 @@
-const shopcontent=document.getElementById("prueba"); //aqui muestra el div prueba del html , coge el elemento por id//
+//const shopcontent=document.getElementById("prueba"); //aqui muestra el div prueba del html , coge el elemento por id//
 const queso=[  //1ero creamos el array un json que dentro va el id y el nombre
     {
         id:1,
-        nombre:"hola",
-        images : "img/usuario.png",
-
-        
+        nombre:"Queso rojo de cabra",
+        descripcion : "Se hace con leche entera,sin pasteurizar, de gran riqueza en mantequilla, procedente de la raza bovina auctóctona dela montaña.",
+        foto : "img/quesorojocabra.jpg",
+        precio: 9.10,
+        categoria:"a",    
     },
-    {
-        id:2,
-        nombre:"adios",
-        images : "img/francia.png",
+    { id:2,
+        nombre:"Queso oveja bredo",
+        descripcion : "Se hace con leche entera,sin pasteurizar, de gran riqueza en mantequilla, procedente de la raza bovina auctóctona dela montaña.",
+        foto : "img/quesoovejaabredo.jpg",
+        precio: 9.10 ,
+        categoria:"b",
+       
+    },
+    {id:3,
+        nombre:"Queso maduro tres leches",
+        descripcion : "Se hace con leche entera,sin pasteurizar, de gran riqueza en mantequilla, procedente de la raza bovina auctóctona dela montaña.",
+        foto : "img/quesomadurotresleches.jpg",
+        precio: 9.10 ,
+        categoria:"c",
+    },
+    {id:4,
+        nombre:"Queso oveja la collada",
+        descripcion : "Se hace con leche entera,sin pasteurizar, de gran riqueza en mantequilla, procedente de la raza bovina auctóctona dela montaña.",
+        foto : "img/quesodeovjealacollada.jpg",
+        precio: 9.10 ,
+        categoria:"d",
+    },
+    {id:5,
+        nombre:"Queso de coan",
+        descripcion : "Se hace con leche entera,sin pasteurizar, de gran riqueza en mantequilla, procedente de la raza bovina auctóctona dela montaña.",
+        foto : "img/quesodecoan.jpg",
+        precio: 9.10 ,
+        categoria:"e",
+    },
+    {id:6,
+        nombre:"Queso de cabrales",
+        descripcion : "Se hace con leche entera,sin pasteurizar, de gran riqueza en mantequilla, procedente de la raza bovina auctóctona dela montaña.",
+        foto : "img/quesocabrales.jpg",
+        precio: 9.10 ,
+        categoria:"f",
+    },
+    {id:7,
+        nombre:"Queso azul de vaca",
+        descripcion : "Se hace con leche entera,sin pasteurizar, de gran riqueza en mantequilla, procedente de la raza bovina auctóctona dela montaña.",
+        foto : "img/quesoazulvaca.jpg",
+        precio: 9.10 ,
+        categoria:"g",
+    },
+    {id:8,
+        nombre:"Queso fuega al pitu",
+        descripcion : "Se hace con leche entera,sin pasteurizar, de gran riqueza en mantequilla, procedente de la raza bovina auctóctona dela montaña.",
+        foto : "img/quesofuegaalpitu.jpg",
+        precio: 9.10 ,
+        categoria:"h",
     },
 ];
-queso.forEach((product)=>{ //recorremos todo el array por el foreach y cada vez que recorremos el array nos crean en htm un h3 y le metemos el nombre del producto
-    let content=document.createElement("div");
+//queso.forEach((product)=>{ //recorremos todo el array por el foreach y cada vez que recorremos el array nos crean en htm un h3 y le metemos el nombre del producto
+  //  let content=document.createElement("div");
     //content.innerHTML=`<h3>${product.id}</h3>
-    content.innerHTML=`<img src ="${product.images}"> 
-    `; //creando etiqueta imagen . 
-shopcontent.append(content); //el resultado que te da es para meterlo en un div padre que se crea en html primero, se ve en inspeccionar elemento 
-});
+    //content.innerHTML=`<img src ="${product.images}"> 
+    //`; //creando etiqueta imagen . 
+//shopcontent.append(content); //el resultado que te da es para meterlo en un div padre que se crea en html primero, se ve en inspeccionar elemento 
+//});
 
 
 //ESTO ES UNA PRUEBA //
